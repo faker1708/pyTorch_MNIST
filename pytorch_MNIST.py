9e9edba2860733a61a5b56bf44b1dac0e83347d0
faker1708 <faker1708@gmail.com>
2023-05-28 17:34:15 +0800

    a

--- pytorch_MNIST.py
+++ pytorch_MNIST.py
@@ -29,11 +29,9 @@ def preprocess_data():
 
 
 ################# LeNet module #################
-class LeNet(nn.Module):
+class full_connect(nn.Module):
     def __init__(self):
-        super(LeNet, self).__init__()
-        self.conv1 = nn.Conv2d(1, 20, 5, 1)
-        self.conv2 = nn.Conv2d(20, 50, 5, 1)
+        super(full_connect, self).__init__()
 
 
 
@@ -42,16 +40,29 @@ class LeNet(nn.Module):
         self.fc2 = nn.Linear(500, 10)
 
     def forward(self, x):
-        # print(x.shape)
-        # x = F.relu(self.conv1(x))
-        # x = F.max_pool2d(x, 2, 2)
-        # x = F.relu(self.conv2(x))
-        # x = F.max_pool2d(x, 2, 2)
-        # x = x.view(-1, 4*4*50)
         x = x.view(-1, 28*28)
+        x = F.relu(self.fc1(x))
 
-        # print(x.shape)
+        x = self.fc2(x)
+        x = F.log_softmax(x, dim=1)
 
+        return x
+
+################# LeNet module #################
+class LeNet(nn.Module):
+    def __init__(self):
+        super(LeNet, self).__init__()
+        self.conv1 = nn.Conv2d(1, 20, 5, 1)
+        self.conv2 = nn.Conv2d(20, 50, 5, 1)
+        self.fc1 = nn.Linear(4*4*50, 500)
+        self.fc2 = nn.Linear(500, 10)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = x.view(-1, 4*4*50)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         x = F.log_softmax(x, dim=1)
@@ -138,6 +149,7 @@ if __name__ == '__main__':
     data_loader_train, data_loader_test = preprocess_data()
 
     ################# LeNet module #################
+    # model = full_connect()
     model = LeNet()
     
     ########## 这里打印的model结构是LeNet 的 __init__ 下的结构 #########
